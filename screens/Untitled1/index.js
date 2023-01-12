707c025772b08bd66c78a06920b37b72bdd5c7e1
Ravinder sales <ravinder.y@crowdbotics.com>
2023-01-12 17:14:07 +0530

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled1/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { StyleSheet, SafeAreaView } from "react-native";
+
+const Untitled1 = ({
+  navigation
+}) => {
+  return <SafeAreaView style={styles.safeArea}>
+        
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  jUaPaGeo: {
+    height: 122,
+    width: 190.5,
+    backgroundColor: "#761f1f",
+    borderRadius: 0,
+    color: "#777777",
+    position: "absolute",
+    top: 205,
+    left: 0,
+    borderColor: "#a63a3a",
+    transform: "rotate(360deg)",
+    borderWidth: 8
+  },
+  gcVTFxZB: {
+    width: 80,
+    height: 80,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 40,
+    transform: "rotate(360deg)",
+    top: 0,
+    left: 0,
+    position: "absolute"
+  },
+  asCynKcv: {
+    position: "relative",
+    opacity: 1
+  }
+});
+export default Untitled1;
