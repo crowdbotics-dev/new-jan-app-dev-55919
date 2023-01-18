bb4a8a3b54514447b4aa919e5b98cfeecfe04a02
Ravinder sales <ravinder.y@crowdbotics.com>
2023-01-18 17:44:47 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled5/index.js
+++ screens/Untitled5/index.js
@@ -1,3 +1,5 @@
+import { RadioGroup } from "react-native-radio-buttons-group";
+import { CheckBox } from "react-native-elements";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
@@ -8,7 +10,15 @@ const Untitled5 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><CheckBox style={styles.QwjFLHkN} title="Checkbox Title"></CheckBox><CheckBox style={styles.XXqjnLDF} title="Checkbox Title"></CheckBox><RadioGroup style={styles.QbGYHdYz} radioButtons={[{
+        id: "1",
+        label: "Option 1",
+        value: "option1"
+      }, {
+        id: "2",
+        label: "Option 2",
+        value: "option2"
+      }]} layout="row"></RadioGroup></ScrollView>
     </SafeAreaView>;
 };
 
@@ -16,6 +26,21 @@ const styles = StyleSheet.create({
   safeArea: {
     height: '100%',
     backgroundColor: "#d42626"
+  },
+  QwjFLHkN: {
+    width: 183,
+    height: 69
+  },
+  XXqjnLDF: {
+    width: 183,
+    height: 69
+  },
+  QbGYHdYz: {
+    width: 120,
+    height: 70,
+    top: 8,
+    left: 5,
+    transform: "rotate(8deg)"
   }
 });
 export default Untitled5;
